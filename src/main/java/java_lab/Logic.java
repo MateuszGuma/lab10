0ad1ba281e5cb38c09d677f85e3a0f1bbccaa103
MrTweety <kiurshi@gmail.com>
2018-06-17 13:01:30 +0200

    Action

--- src/main/java/java_lab/Logic.java
+++ src/main/java/java_lab/Logic.java
@@ -1,4 +1,73 @@
 package java_lab;
 
+//przykładowe wywołanie
+/*        Logic l = new Logic();
+                System.out.println(l.convert(Logic.Convert.fromArabicToRoman, "10"));
+                System.out.println(l.convert(Logic.Convert.fromArabicToUrnfield, "10"));
+                System.out.println(l.convert(Logic.Convert.fromRomanToArabic, "MMCC"));
+                System.out.println(l.convert(Logic.Convert.fromUrnfieldToArabic, "//\\"));*/ //return 7
+
+
 public class Logic {
+
+    public enum Convert {
+        fromUrnfieldToArabic, fromArabicToUrnfield, fromRomanToArabic, fromArabicToRoman;
+    }
+
+
+    public String convert(Convert  en, String number){
+        String result;
+
+
+        //Convert t = Convert.valueOf()[en];
+
+        switch (en) {
+            case fromUrnfieldToArabic:
+                UrnfieldNumber fromUrnfie =  new UrnfieldNumber();
+                try {
+                    result = String.valueOf(fromUrnfie.urnfieldToArabic(number));
+
+                }catch (IllegalArgumentException ee){
+                    result = "IllegalArgument";
+                }
+                break;
+
+            case fromArabicToUrnfield:
+                UrnfieldNumber ToUrnfield =  new UrnfieldNumber();
+                try {
+                    result = ToUrnfield.arabicToUrnfield(Integer.parseInt(number));
+
+                }catch (IllegalArgumentException ee){
+                    result = "IllegalArgument";
+                }
+                break;
+
+            case fromRomanToArabic:
+                RomanNumber fromRoman =  new RomanNumber();
+                try {
+                    result = String.valueOf(fromRoman.romanToArabic(number));
+
+                }catch (IllegalArgumentException ee){
+                    result = "IllegalArgument";
+                }
+                break;
+
+            case fromArabicToRoman:
+                RomanNumber ToRoman =  new RomanNumber();
+                try {
+                    result = ToRoman.arabicToRoman(Integer.parseInt(number));
+
+                }catch (IllegalArgumentException ee){
+                    result = "IllegalArgument";
+                }
+                break;
+
+            default:
+                result = "Convert " + en;
+        }
+        return result;
+    }
+
+  //return result;
+
 }
